25acff4f7a210d3761e897ff636c62e299446e98
藤林　沙耶 <saya@fujibayashisayanoMacBook-Pro.local>
2022-08-21 21:20:28 +0900

    create seeder list

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'company_id' => $faker->company(),
+        'product_name' => $faker->word(),
+        'price' => $faker->numberBetween(100,200),
+        'stock' => $faker->numberBetween(10,20),
+        'comment' => $faker->realText(100),
+        'img_path' => $faker->imageUrl()
+    ];
+});
